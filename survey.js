e71911173c8458bef6f69c8239e5b5ee0e31db61
Camillia Snow <s.camillia@gmail.com>
2023-09-17 20:05:15 -0300

    linted code, nice pyramid/callback hell happening now.

--- survey.js
+++ survey.js
@@ -12,13 +12,13 @@ rl.question('What\'s your name, or nickname? ', (answerName) => {
         rl.question('What\'s you\'re favourite thing to eat for that meal? ', (answer5) => {
           rl.question('What sport is your absolute favourite? ', (answer6) => {
             rl.question('What is your superpower? In a few words, tell us what you are amazing at! ', (answer7) => {
-  console.log(`${answerName} loves listening to ${answer3} while ${answer2}, devouring ${answer5} for ${answer4}, prefers ${answer6} over any other sport, and is amazing at ${answer7}.`);
+              console.log(`${answerName} loves listening to ${answer3} while ${answer2}, devouring ${answer5} for ${answer4}, prefers ${answer6} over any other sport, and is amazing at ${answer7}.`);
 
-  rl.close();
-});
-  });
-     });
-        });
-          });
+              rl.close();
             });
-              });
+          });
+        });
+      });
+    });
+  });
+});
